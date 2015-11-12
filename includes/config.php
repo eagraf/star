892a327afcb2c7aa451795d297d2541520acb47e
eagraf <graf.ethan@gmail.com>
2015-11-12 15:55:40 -0500

    Made a testing framework to make it easier to implement features without interfering with other code.
    Signed-off-by: eagraf <graf.ethan@gmail.com>

--- includes/config.php
+++ includes/config.php
@@ -10,8 +10,9 @@
     session_start();
 	
 	// require authentication for all pages except /login.php, /logout.php, and /register.php
-    if (!in_array($_SERVER["PHP_SELF"], ["/login.php", "/index.php", "/group_create.php", "/group_finalize.php"]))
+    if (!in_array($_SERVER["PHP_SELF"], ["/login.php", "/index.php", "/group_create.php", "/group_finalize.php", "/test.php", "/tests/*"]))
     {
+		//Otherwise redirect to login.
         if (empty($_SESSION["id"]))
         {
             redirect("login.php");
---
+++ views/test_view.php
@@ -0,0 +1,18 @@
+<form action="test.php" method="post">
+	<?php
+		//A list of the test pages, bad practice but it works for now.
+		$tests = [
+			'upload_test' => ['path' => 'tests/upload_test.php', 'name' => 'Upload Test']
+			];
+		
+		//Print a list of buttons that link to each test page.
+		foreach($tests as $test)
+		{
+			print('<div class="form-group">
+				<button class="btn btn-default" type="submit"  name="test_button" value='.$test["path"].'>
+					<p>'.$test["name"].'</p>
+				</button>
+				<div>');
+		} 
+	?>
+</form>
