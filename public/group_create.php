e2941a4fa85b9efd847234f346a60af75566a4c0
Sam Rackey <samrcr71@gmail.com>
2015-11-14 16:17:56 -0500

    -site will now Oops! if someone does not enter a group size
    Signed-off-by: Sam Rackey <samrcr71@gmail.com>

--- public/group_create.php
+++ public/group_create.php
@@ -13,6 +13,10 @@
     // else if user reached page via POST (as by submitting a form via POST)
     else if ($_SERVER["REQUEST_METHOD"] == "POST")
     {	
+		if (empty($_POST["group_size"]))
+        {
+            error("You must specify the size of the group.");
+        }
 		$size = $_POST['group_size'];
 		render("group_finalize_form.php", ["title" => "Register", "size" => $size ]);
 		
