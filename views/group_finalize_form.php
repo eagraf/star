7cd27ccb295474f601bb39746062a27c58c47ad0
Sam Rackey <samrcr71@gmail.com>
2015-11-20 18:53:53 -0500

    messed with indentation
    Signed-off-by: Sam Rackey <samrcr71@gmail.com>

--- views/group_finalize_form.php
+++ views/group_finalize_form.php
@@ -2,22 +2,22 @@
     <fieldset>
         <div class="form-group">
 			<?php
-			for($i = 0; $i < $size; $i++){
-				$name = "name" . $i;
-				$email = "email" . $i;
-				print "<input class=\"form-control\" name=\"$name\" placeholder=\"Name\" type=\"text\"/>";
-				print "<input class=\"form-control\" name=\"$email\" placeholder=\"Email\" type=\"text\"/>";
-				print "<br>";
-			}
+				for($i = 0; $i < $size; $i++){
+					$name = "name" . $i;
+					$email = "email" . $i;
+					print "<input class=\"form-control\" name=\"$name\" placeholder=\"Name\" type=\"text\"/>";
+					print "<input class=\"form-control\" name=\"$email\" placeholder=\"Email\" type=\"text\"/>";
+					print "<br>";
+				}
 			?>
         </div>
 		<div class="form-group">
             <?php
-			for($i = 0; $i < $cat_num; $i++){
-				$category = "category" . $i;
-				print "<input class=\"form-control\" name=\"$category\" placeholder=\"Category\" type=\"text\"/>";
-				print "<br>";
-			}
+				for($i = 0; $i < $cat_num; $i++){
+					$category = "category" . $i;
+					print "<input class=\"form-control\" name=\"$category\" placeholder=\"Category\" type=\"text\"/>";
+					print "<br>";
+				}
 			?>
         </div>
 		<div class="form-group">
