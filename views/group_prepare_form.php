5f5a403792693e94f27bfc4da202308cc06569fe
Sam Rackey <samrcr71@gmail.com>
2015-11-22 17:37:02 -0500

    Client Side verification now exists (checks if input is truly an email)
    Signed-off-by: Sam Rackey <samrcr71@gmail.com>

--- views/group_prepare_form.php
+++ views/group_prepare_form.php
@@ -1,7 +1,7 @@
 <form action="group_create.php" method="post">
     <fieldset>
         <div class="form-group">
-            <input class="form-control" name="group_size" placeholder="Group Size" type="text"/>
+            <input class="form-control" name="group_size" placeholder="Group Size" type="number" min="1" required />
         </div>
         <div class="form-group">
             <button class="btn btn-default" type="submit">
