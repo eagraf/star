ddb45a6bfc76d71ef0e617d09833fc3a4bb0ecc0
Sam Rackey <samrcr71@gmail.com>
2015-11-25 17:07:00 -0500

    -cleaned up category getting, drop-down in board autosubmits when value is selected
    Signed-off-by: Sam Rackey <samrcr71@gmail.com>

--- views/board_view.php
+++ views/board_view.php
@@ -12,7 +12,7 @@
 			<form action="change_group.php" method="post">
 				<fieldset>
 					<div class="form-group">
-						<select class="form-control" name="group">
+						<select class="form-control" name="group" onchange='this.form.submit()'>
 							<?php
 							print("<option disabled selected value=\"\">{$group_id}</option>");
 							
@@ -25,7 +25,6 @@
 						</select>
 					</div>
 					<div class="form-group">
-						<button class="btn btn-default" type="submit">Change Group</button>
 						<a href="group_create.php" class="btn btn-default" role="button">New Group</a>
 					</div>
 				</fieldset>
